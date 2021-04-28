6cf9a33bc6f1c5fa8642094f3e4b11d8fd98b9d0
Mike Shohet <mlshohet@gmail.com>
2021-04-28 01:34:35 -0400

    added and configured expo-sqlite

--- App.js
+++ App.js
@@ -5,10 +5,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
-import placesReducer from './store/places-reducer';
-
 import { NavigationContainer } from '@react-navigation/native';
 import PlacesNavigator from './navigation/PlacesNavigator';
+
+import placesReducer from './store/places-reducer';
+import { init } from './helpers/db';
+
+init().then(() => {
+	console.log('Initialized database.');
+})
+.catch(err => {
+	console.log('Initializing db failed.');
+	console.log(err);
+});
 
 const rootReducer = combineReducers({
 	places: placesReducer,
